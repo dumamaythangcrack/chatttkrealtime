51784cd12df4159f355ae856f8d322bdd259bd97
dumamaythangcrack <khangnek705@gmail.com>
2025-11-30 21:08:43 +0700

    Delete client directory

--- client/public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', event => {
-  const data = event.data?.json() || { title: 'Tin nhắn mới', body: 'Bạn có tin nhắn mới!' };
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: '/icon-192.png'
-  });
-});
